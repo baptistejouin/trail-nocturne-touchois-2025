aac5ed9c2ccafddef874ca44ead4b5367c41b53e
baptistejouin <baptiste.jouin44@gmail.com>
2025-02-01 15:21:28 +0100

    lib: update tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
   experimental: {
     svg: true,
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
