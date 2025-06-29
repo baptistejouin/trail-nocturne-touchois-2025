b3e6ccc56b0db58d965169022e3ec18a2de29275
baptistejouin <baptiste.jouin44@gmail.com>
2025-06-29 22:48:40 +0200

    prod 2025

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    svg: true,
-  },
-
   vite: {
     plugins: [tailwindcss()],
   },
